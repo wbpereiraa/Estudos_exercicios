20203e73d6de3368649474b0ad9b851001e0bb7c
William Barbosa <wb.pereiraa@gmail.com>
2023-10-25 08:20:44 -0300

    Adicionando modificadores de acesso/Métodos Get e Set/Construtores(sobrecarga)

--- controleDeEstoque/src/application/Program.java
+++ controleDeEstoque/src/application/Program.java
@@ -9,15 +9,17 @@ public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Product product = new Product();
+
         System.out.println("Enter product data: ");
         System.out.print("Name: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
 
         System.out.print("Price: ");
-        product.price = sc.nextDouble();
+        double price = sc.nextDouble();
         System.out.print("Quantity in stock: ");
-        product.quantity = sc.nextInt();
+        int quantity = sc.nextInt();
+
+        Product product = new Product(name, price, quantity);
 
         System.out.println();
 
@@ -26,7 +28,7 @@ public class Program {
         System.out.println();
 
         System.out.print("Enter the number of products to be added in stock: ");
-        int quantity = sc.nextInt();
+        quantity = sc.nextInt();
         product.addProducts(quantity);
 
         System.out.println();
